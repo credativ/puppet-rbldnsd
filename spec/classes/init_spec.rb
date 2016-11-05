f82c5ceebd21870c10616bfac701f244b75d4216
Arnd Hannemann <arnd.hannemann@credativ.de>
2016-11-05 12:49:47 +0100

    fixup simple compile test

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'rbldnsd' do
   context 'with default values' do
     it do
-      is_expected.to contain_compile
+      is_expected.to compile.with_all_deps
     end
   end
 end
